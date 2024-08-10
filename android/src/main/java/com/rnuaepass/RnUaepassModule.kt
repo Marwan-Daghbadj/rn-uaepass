dfe06ed3a43c1c0fde3ea0c5674062354e5d4c59
Marwan Daghbadj <daghbadjmarwan@gmail.com>
2024-08-10 22:06:54 +0400

    add isAppInstalled to package

--- android/src/main/java/com/rnuaepass/RnUaepassModule.kt
+++ android/src/main/java/com/rnuaepass/RnUaepassModule.kt
@@ -1,9 +1,10 @@
-package com.rnuaepass
+package com.rn.uaepass;
 
-import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Promise
+import android.content.pm.PackageManager;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
 
 class RnUaepassModule(reactContext: ReactApplicationContext) :
   ReactContextBaseJavaModule(reactContext) {
@@ -12,13 +13,20 @@ class RnUaepassModule(reactContext: ReactApplicationContext) :
     return NAME
   }
 
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
   @ReactMethod
   fun multiply(a: Double, b: Double, promise: Promise) {
     promise.resolve(a * b)
   }
-
+  @ReactMethod
+  fun isAppInstalled(packageName: String, successCallback: Callback, errorCallback: Callback) {
+      try {
+          val packageManager = reactApplicationContext.packageManager
+          packageManager.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES)
+          successCallback.invoke(true)
+      } catch (e: PackageManager.NameNotFoundException) {
+          successCallback.invoke(false)
+      }
+  }
   companion object {
     const val NAME = "RnUaepass"
   }
